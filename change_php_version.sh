0db5e995902cab92bd56df7898451d4ad232089f
Pushkraj Jori <pushkrajjori19@gmail.com>
2023-08-27 10:11:49 +0530

    Update change_php_version.sh

--- change_php_version.sh
+++ change_php_version.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
-# Array of available PHP versions
-php_versions=("lsphp74" "lsphp80" "lsphp82")  # Update with your available versions
+# List of available PHP versions
+php_versions="lsphp74 lsphp80 lsphp82"  # Update with your available versions
 
 # Display menu of PHP versions
 echo "Select the PHP version you want to set as default:"
-for ((i=0; i<${#php_versions[@]}; i++)); do
-    echo "$i. ${php_versions[i]}"
+i=0
+for php_version in $php_versions; do
+    echo "$i. $php_version"
+    i=$((i + 1))
 done
 
 # Get user's choice
 read -p "Enter the number corresponding to the PHP version: " choice
 
 # Validate the choice
-if [[ $choice =~ ^[0-9]+$ ]] && (( choice >= 0 && choice < ${#php_versions[@]} )); then
-    selected_version="${php_versions[choice]}"
+if [ "$choice" -ge 0 ] && [ "$choice" -lt $i ]; then
+    selected_version=$(echo $php_versions | cut -d " " -f $(($choice + 1)))
     echo "Changing PHP version to $selected_version..."
 
     # Remove existing PHP binary symlink if it exists
